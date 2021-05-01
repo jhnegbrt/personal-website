db4da171518ec6a0f3ae495bc019cc5657e31349
jhnegbrt <jhnegbrt@gmail.com>
2021-05-01 15:07:10 -0700

    adds about me text and image

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -12,12 +12,20 @@ class About extends React.Component{
         <img id="picture" src="/ketchikan.JPEG" />
         <div className="about-text">
           <h1>Jack Egbert</h1>
+          <h2>Software Engineering - Web Design & Development</h2>
           <div className="info-text">
-            <h2>Software Engineering</h2>
-            <h2>Web Design & Development</h2>
+            <h2>A bit about me:</h2>
             <h3>My definition of fun:</h3>
             <h3>Working with others, or for others, to bring things into
               the world that didn't exist before
+            </h3>
+            <h3>What makes me tick:</h3>
+            <h3>Solving problems that require extending the boundaries
+              of my knowledge
+            </h3>
+            <h3>My favorite projects:</h3>
+            <h3>Found novel use cases for familiar technology or 
+              required learning and utilizing an unfamiliar technology
             </h3>
           </div>
         </div>
