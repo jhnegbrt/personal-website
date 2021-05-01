50a24ea9fe6eb086554acef8be70481927158a88
jhnegbrt <jhnegbrt@gmail.com>
2021-05-01 12:00:39 -0700

    adds desktop navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import './styles/App.scss'
+import './styles/Reset.css'
 import Navbar from './components/navbar'
 // import MobileNavbar from './components/navbar_mobile'
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Navbar />
       <div>My App</div>
     </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,17 @@
+import '../styles/Navbar.scss'
+
 function Navbar(){
   return(
-    <div className="nav">
-      <div>
-        <h3>Jack Egbert</h3>
-      </div>
-      <div className="nav-items">
-        <ul>
-          <li>Projects</li>
-          <li>Resume</li>
-          <li>Skills</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-    </div>
+    <header>
+      <h3>Jack Egbert</h3>
+      <ul>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Resume</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
+    </header>
   )
 }
 
