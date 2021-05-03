8d22a9b706488779b89e772a0e91db58768bb076
jhnegbrt <jhnegbrt@gmail.com>
2021-05-03 13:24:38 -0700

    stashing changes

--- src/components/welcome_modal.jsx
+++ src/components/welcome_modal.jsx
@@ -4,27 +4,25 @@ import '../styles/WelcomeModal.scss'
 class WelcomeModal extends React.Component{
   constructor(props){
     super(props)
-    this.state = {
-      welcomeModal: true
-    }
   }
 
   render(){
     debugger
     let welcomeModal = (
       <div className={this.props.className ? "welcomeModal off-screen" : "welcomeModal"}>
-        <div className="block-1">
-          <button onClick={this.props.closeModal}>Enter</button>
-        </div>
+        <div className="block-1"></div>
+        <div className="modal-intro">
+            {/* <h2>Welcome</h2> */}
+            {/* <h3>johnwegbert.com</h3> */}
+            <button onClick={this.props.closeModal}>Enter</button>
+          </div>
         <div className="block-2"></div>
         <div className="block-3"></div>
         <div className="block-4"></div>
-        {/* <h2>Welcome</h2>
-        <h3>johnwegbert.com</h3> */}
       </div>
     )
     return(
-      this.state.welcomeModal === true ? welcomeModal : ""
+      welcomeModal
     )
   }
 }
